5e05479b9ed3fd60debb8cc2730f31a0c6f7a02d
Pooodgie <99168514+Pooodgie@users.noreply.github.com>
2022-02-14 11:37:25 +0100

    feb 14

--- javascript.js
+++ javascript.js
@@ -14,14 +14,27 @@ function getVars() {
 
     start(odds, succeeds, numAVG, displayRolls);
 }
+function median(numbers) {
+    const sorted = numbers.slice().sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
+
+    if (sorted.length % 2 === 0) {
+        return (sorted[middle - 1] + sorted[middle]) / 2;
+    }
+
+    return sorted[middle];
+}
+
 
 function start(odds, success_desired, numAVG, displayRolls) {
+    var nums = [];
     function success() {
         numberOfSuccesses++;
     }
     function fail() {
         numberOfSuccesses = 0;
     }
+
     for (var i = 0; i < numAVG; i++) {
         numRolls[i] = 0;
         function runRNG() {
@@ -38,8 +51,9 @@ function start(odds, success_desired, numAVG, displayRolls) {
             runRNG();
         }
         if (numberOfSuccesses >= success_desired) {
+            nums[i] = numRolls[i];
             if (displayRolls == 2) {
-                document.getElementById("rollspam").innerHTML += ("To hit a " + odds * 100 + "% chance " + success_desired + " times in a row required " + (numRolls[i] - 1) + " rolls");
+                document.getElementById("rollspam").innerHTML += ("To hit a " + odds * 100 + "% chance " + success_desired + " times in a row required <span id='weird'>" + (numRolls[i]) + "</span> rolls");
             }
 
             if (displayRolls == 2) {
@@ -47,14 +61,11 @@ function start(odds, success_desired, numAVG, displayRolls) {
             }
             numberOfSuccesses = 0;
             console.log(numRolls[i]);
-            runRNG();
         }
     }
     for (var i = 0; i < numRolls.length; i++) {
         arrayavg = arrayavg + (numRolls[i]);
     }
-    var avg = Math.round(arrayavg / numRolls.length);
-    document.getElementById("paste").innerHTML = ("The average odds across " + numRolls.length + " rolls was one in " + avg + "<br>");
-
-
+    var avg = Math.round(median(nums));
+    document.getElementById("paste").innerHTML = ("The median odds across " + numRolls.length + " rolls was one in " + avg + "<br>");
 }
